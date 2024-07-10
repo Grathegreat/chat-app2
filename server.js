af526ac4848079932db01ee06b2a4d3642ad3c7d
Grathegreat <onlyonehacker999@gmail.com>
2024-07-10 13:36:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const users = {}; // Object to store usernames
-const inappropriateWords = ['admin', 'moderator', 'root', 'author']; // Example list of inappropriate words
-
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
@@ -16,11 +13,6 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
-        const username = users[socket.id];
-        if (username) {
-            delete users[socket.id];
-            io.emit('user left', username);
-        }
     });
 
     socket.on('chat message', (msg) => {
@@ -28,39 +20,9 @@ io.on('connection', (socket) => {
     });
 
     socket.on('user joined', (username) => {
-        if (!isUsernameAvailable(username)) {
-            socket.emit('username taken', username);
-            return;
-        }
-
-        if (isInappropriateUsername(username)) {
-            socket.emit('the admin not allow to use this username!', username);
-            return;
-        }
-
-        users[socket.id] = username;
         io.emit('user joined', username);
     });
 });
-
-function isUsernameAvailable(username) {
-    for (let socketId in users) {
-        if (users[socketId] === username) {
-            return false;
-        }
-    }
-    return true;
-}
-
-function isInappropriateUsername(username) {
-    const lowerUsername = username.toLowerCase();
-    for (let word of inappropriateWords) {
-        if (lowerUsername.includes(word)) {
-            return true;
-        }
-    }
-    return false;
-}
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
