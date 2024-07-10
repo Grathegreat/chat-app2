b1937c873da73fda257b33fadd1d19a286e93325
Grathegreat <onlyonehacker999@gmail.com>
2024-07-10 12:40:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const users = {}; // Object to store usernames
+
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
@@ -13,6 +15,11 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        const username = users[socket.id];
+        if (username) {
+            delete users[socket.id];
+            io.emit('user left', username);
+        }
     });
 
     socket.on('chat message', (msg) => {
@@ -20,9 +27,23 @@ io.on('connection', (socket) => {
     });
 
     socket.on('user joined', (username) => {
-        io.emit('user joined', username);
+        if (isUsernameAvailable(username)) {
+            users[socket.id] = username;
+            io.emit('user joined', username);
+        } else {
+            socket.emit('username taken', username);
+        }
     });
 });
+
+function isUsernameAvailable(username) {
+    for (let socketId in users) {
+        if (users[socketId] === username) {
+            return false;
+        }
+    }
+    return true;
+}
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
